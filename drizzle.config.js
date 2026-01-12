88d7f88187533a55c46a3481df7256b802d9056c
huzaifa <your_email@example.com>
2026-01-12 12:10:38 +0500

    fix done

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: './utils/Schema.js',
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgresql://AI_InterView_DB_owner:6MJ7oFcDIfhY@ep-plain-wave-a52a6qgx.us-east-2.aws.neon.tech/AI_InterView_DB?sslmode=require',
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL,
   },
 }
